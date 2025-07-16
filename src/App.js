d29e103d8c5668549179b5898effdb6fb2d966ae
Giuseppe Funaro <139779768+Geppix140269@users.noreply.github.com>
2025-07-16 07:14:16 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ItalianTaxFormService from './components/ItalianTaxFormService';
+
+function App() {
+  return (
+    <div className="App">
+      <ItalianTaxFormService />
+    </div>
+  );
+}
+
+export default App;
